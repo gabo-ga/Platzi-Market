44294d2eb2e4cddda02cd623dd970dfafa78ce05
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-05-28 22:41:17 -0400

    feat:clase cliente

---
+++ src/main/java/com/platzi_market/persistence/entity/Cliente.java
@@ -0,0 +1,74 @@
+package com.platzi_market.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @Column(name = "id")
+    private String idCliente;
+
+    private String nombre;
+
+    @Column(name = "apellidos")
+    private String apellido;
+
+    private Integer celular;
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
