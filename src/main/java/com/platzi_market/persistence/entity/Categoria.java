f8a17c11a8821d067df2af2a4bff812453a1e818
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-05-28 22:35:32 -0400

    feat:clase categoria

---
+++ src/main/java/com/platzi_market/persistence/entity/Categoria.java
@@ -0,0 +1,16 @@
+package com.platzi_market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String descripcion;
+    private Boolean estado;
+}
