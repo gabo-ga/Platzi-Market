8060ee94ada1da437dd4e177641d5e7c86dceff5
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-05-28 23:05:38 -0400

    feat:relaciones de la bd

--- src/main/java/com/platzi_market/persistence/entity/Compra.java
+++ src/main/java/com/platzi_market/persistence/entity/Compra.java
@@ -3,6 +3,7 @@ package com.platzi_market.persistence.entity;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name="compras")
@@ -26,6 +27,9 @@ public class Compra{
     @ManyToOne
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
+
+    @OneToMany(mappedBy = "compra")
+    private List<ComprasProducto> productos;
 
     public Integer getIdCompra() {
         return idCompra;
