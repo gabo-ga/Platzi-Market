bee3788059b3b72228ad3f67b1fdc255c4961958
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-05-30 17:49:51 -0400

    feat:mapper terminados

--- src/main/java/com/platzi_market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi_market/persistence/entity/ComprasProducto.java
@@ -10,9 +10,27 @@ public class ComprasProducto {
 
     private Integer cantidad;
     private Double total;
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
     private Boolean estado;
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false, updatable = false)
     private Compra compra;
 
